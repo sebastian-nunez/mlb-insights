4db1ced7d24c8eeb751b6e554b99c66088fe97f6
sebastian-nunez <02snunez@gmail.com>
2023-04-08 02:36:20 -0400

    version 1: missing individual player analytics

--- app.py
+++ app.py
@@ -7,18 +7,81 @@ import statsapi
 import requests
 from collections import defaultdict
 from enum import Enum
+from streamlit_folium import folium_static
+import folium
+import json
+from datetime import datetime
+import altair as alt
+
+st.set_page_config(
+    page_title="MLB Insights",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': '''
+        MLB Insights is the ultimate site for baseball fans who want to stay up-to-date on the latest player statistics and profiles.
+
+        **Developed by:** Sebastian Nunez (2023)
+
+        Did you know that we have an `easter egg` *hidden* somewhere on the site? To find it, you'll need to use your skills as a detective and delve into the **HTML source code**. Keep your eyes peeled for any clues that might lead you to the hidden gem. Who knows what kind of insights or fun surprises you might uncover? Happy hunting!
+        '''
+    }
+)
+
+player = None
+BALLPARKS_URL = 'https://raw.github.com/mlbscorebot/ballparks/master/data/ballparks.json'
+
+
+class Page(Enum):
+    WHY_US = 'Why Us?'
+    PLAYER_SEARCH = 'Player Search'
+    LEAGUE_LEADERS = 'League Leaders'
+    BALLPARKS = 'Ball Parks'
 
 
 def main():
-    st.title('MLB Insights')
-    st.caption('MLB Insights is the ultimate site for baseball fans who want to stay up-to-date on the latest player statistics and profiles.')
-    st.divider()
+    # default page
+    page = Page.PLAYER_SEARCH.value
+
+    # sidebar
+    with st.sidebar:
+        page = option_menu("MLB Insights", [Page.PLAYER_SEARCH.value, Page.LEAGUE_LEADERS.value, Page.BALLPARKS.value, Page.WHY_US.value, ],
+                           icons=['search', 'list-task', 'geo-alt', 'info-circle'], menu_icon="body-text", default_index=0)
+
+    # main content
+    if page == Page.PLAYER_SEARCH.value:
+        st.title('MLB Insights')
+        st.caption('MLB Insights is the ultimate site for baseball fans who want to stay up-to-date on the latest player statistics and profiles.')
+        st.divider()
+
+        display_player_search()
+    elif page == Page.LEAGUE_LEADERS.value:
+        st.header(Page.LEAGUE_LEADERS.value)
+        st.divider()
+
+        display_league_leaders()
+    elif page == Page.BALLPARKS.value:
+        st.header(Page.BALLPARKS.value)
+        st.caption('You can find all the MLB stadiums on this map!')
+        st.divider()
+
+        display_ballparks()
+    elif page == Page.WHY_US.value:
+        st.title('MLB Insights')
+        st.caption('MLB Insights is the ultimate site for baseball fans who want to stay up-to-date on the latest player statistics and profiles.')
+        st.divider()
+
+        display_benefits()
+    else:
+        page = Page.PLAYER_SEARCH.value
+
+
+def display_player_search():
+    global player
 
     players = defaultdict(dict)  # id -> Player()
     name_to_id = defaultdict(str)  # name -> id
     name_to_id[''] = None
-
-    player = None
 
     all_players = statsapi.lookup_player("")
     if not all_players:
@@ -31,29 +94,131 @@ def main():
         players[id] = Player(player)
         name_to_id[name] = id
 
-    page = Page.PLAYER_SEARCH.value  # default page
-    with st.sidebar:
-        page = option_menu("MLB Insights", [Page.PLAYER_SEARCH.value, Page.LEAGUE_LEADERS.value],
-                           icons=['search', 'list-task'], menu_icon="body-text", default_index=0)
+    # sort names alphabetically
+    name_to_id = dict(sorted(name_to_id.items(), key=lambda x: x[0]))
 
-    if page == Page.PLAYER_SEARCH.value:
-        player_name = st.selectbox(
-            label='Search for a player',
-            options=name_to_id.keys(),
-            help="Enter a player's name then press ENTER...")
+    st.header(Page.PLAYER_SEARCH.value)
 
-        if player_name != None:
-            id = name_to_id[player_name]
-            player = players[id]
+    player_name = st.selectbox(
+        label='Search for a player',
+        options=name_to_id,
+        help="Enter a player's name then press ENTER...")
 
-        if player:
-            st.success(f'You Selected: {player}')
-            display_player_info(player)
+    if player_name != None:
+        id = name_to_id[player_name]
+        player = players[id]
 
-    elif page == Page.LEAGUE_LEADERS.value:
-        pass
+    if player:
+        st.success(f'You Selected: {player}')
+        display_player_info(player)
+
+
+def display_league_leaders():
+    options = {
+        'Homeruns': 'homeruns',
+        'ERA': 'earnedRunAverage',
+        'OPS': 'onBasePlusSlugging',
+        'Errors': 'errors',
+    }
+
+    # Parameters
+    metric = st.radio("Select a metric:", options.keys())
+
+    seasons = [year for year in range(datetime.now().year, 1971, -1)]
+    selected_season = st.selectbox('Select a season:', seasons)
+
+    max_results = st.slider('Select the number of results:', 5,
+                            100, step=5, value=10)
+
+    # data fetching
+    data = statsapi.league_leader_data(metric, season=selected_season, limit=max_results, statGroup=None,
+                                       leagueId=None, gameTypes=None, playerPool=None, sportId=1, statType=None)
+
+    df = pd.DataFrame(data, columns=['Rank', 'Player', 'Team', metric])
+
+    hide_table_row_index = """
+        <style>
+        thead tr th:first-child {display:none}
+        tbody th {display:none}
+        </style>
+        """
+
+    # Inject CSS with Markdown
+    st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+    st.subheader(
+        f'Top {max_results} Players by {metric} ({selected_season})')
+
+    table_tab, chart_tab = st.tabs(['Table', 'Chart'])
+
+    with table_tab:
+        st.table(df)
+
+    with chart_tab:
+
+        # horizontal bar chart using Altair
+        chart = alt.Chart(df).mark_circle().encode(
+            y=alt.Y(metric, sort='-y'),
+            x=alt.X('Player', sort='-x'),
+            color='Team'
+        ).properties(
+            width=700,
+            height=400,
+
+        )
+
+        # display the chart in Streamlit
+        st.altair_chart(chart, use_container_width=True)
+
+
+def display_ballparks():
+    res = requests.get(
+        BALLPARKS_URL)
+
+    if res.status_code == 200:
+        data = res.json()
+
+        map = folium.Map(location=[40, -95], zoom_start=4)
+
+        for name, park in data.items():
+            lat = park["lat"]
+            long = park["long"]
+            team = park["team"]
+            popup_text = f"{name} ({team})"
+
+            folium.Marker(location=[lat, long],
+                          popup=popup_text).add_to(map)
+
+        folium_static(map)
     else:
-        page = Page.PLAYER_SEARCH.value
+        st.error(
+            'Unable to fetch Ballpark locations! (fetching from {BALLPARKS_URL})')
+
+
+def display_benefits():
+
+    st.markdown(
+        f'''
+        ## Benefits
+
+        1. **Easy-to-use interface:** The site is designed with an easy-to-use interface, so you can quickly find the information you need.
+
+        2. **Search functionality:** MLB Insights has a powerful search functionality that allows you to search for specific players, teams, or games.
+
+        3. **Perfect for fantasy baseball:** If you're a serious fantasy baseball player, MLB Insights is the perfect resource to help you make informed decisions about your team.
+
+        4. **Comprehensive player profiles:** With detailed player profiles, you can get a complete picture of a player's performance history, strengths, weaknesses, and more.
+
+        5. **Available 24/7:** MLB Insights is always available, so you can access the site whenever you need it, day or night.
+
+        6. **Free to use:** MLB Insights is completely free to use, so you don't have to worry about paying for a subscription or membership.
+        '''
+    )
+
+    easter_egg = st.checkbox('Check me... But how? Hmmmmm', disabled=True)
+    if easter_egg:
+        st.success('Congratulations! You have found the easter egg on the MLB Insights website! Your sharp detective skills have paid off and we are thrilled that you were able to uncover our hidden gem!')
+        st.balloons()
 
 
 def display_player_info(player):
@@ -69,42 +234,47 @@ def display_player_info(player):
 
         city, state, country = info['birth_city'], info['birth_state'], info['birth_country']
         birth_location = ''
-        if city:
-            birth_location = f'{city}, {country}'
-        elif state:
-            birth_location = f'{state}, {country}'
+        if country:
+            if city and state:
+                birth_location = f'{city}, {state}, {country}'
+            elif city and not state:
+                birth_location = f'{city}, {country}'
+            elif state and not city:
+                birth_location = f'{state}, {country}'
+        else:
+            birth_location = 'Unknown'
 
         height = f"{info['height_feet']}' {info['height_inches']}\""
-
         st.markdown(
             f'''
             # {player.name} ({player.position}) - :red[#{player.number}]
+            #### {info['team_name']}
             > **Age:** {info['age']} | **Height:** {height} | **Weight:** {info['weight']}lbs | **Bats/Throws:** {info['bats']}/{info['throws']} | **Status:** {info['status']}
             >
-            > **Team:** {info['team_name']}
+            > **Born:** {birth_location}
             ''')
 
-        tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+        career_hitting, career_pitching, projected_hitting, projected_pitching = st.tabs(
+            ["Career Hitting", "Career Pitching", "Projected Hitting", "Projected Pitching"])
 
-        with tab1:
+        with career_hitting:
             st.header("A cat")
             st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
-        with tab2:
+        with career_pitching:
+            st.header("A cat")
+            st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+        with projected_hitting:
             st.header("A dog")
             st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
-        with tab3:
-            st.header("An owl")
-            st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+        with projected_pitching:
+            st.header("A dog")
+            st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
     else:
         st.error(f'Unable to fetch full player info for {player.name}!')
-
-
-class Page(Enum):
-    PLAYER_SEARCH = 'Player Search'
-    LEAGUE_LEADERS = 'League Leaders'
 
 
 class Player:
